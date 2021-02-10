7ad1dd5b96df68a1bb320d5b3ace5381cc4374f5
Glitch (catgirl-prodv2) <none>
2021-02-10 00:22:08 +0000

    Version 1.8

--- status.js
+++ status.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 bot.on("ready", () => {
   console.log("Catgirl is outputting a status!");
-  bot.user.setActivity("Digital Girl by Hatsune Miku 💙", { type: "LISTENING" });
+  bot.user.setActivity("Doki Doki Literature Club", { type: "PLAYING" });
 });
 
 bot.login(process.env.DISCORD_TOKEN);
